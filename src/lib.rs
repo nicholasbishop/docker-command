a40d408a3c8be13f767943887a10420b3ea8ebf7
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-05 16:01:52 -0400

    Add volumes

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,13 @@ impl Docker {
         if let Some(name) = &opt.name {
             cmd.add_arg_pair("--name", name);
         }
+
+        // Add volumes
+        for vol in &opt.volumes {
+            cmd.add_arg_pair("--volume", vol.arg());
+        }
+
+        // Add image and command+args
         cmd.add_arg(opt.image);
         if let Some(command) = &opt.command {
             cmd.add_arg(command);
@@ -86,13 +93,54 @@ pub struct BuildOpt {
     pub tag: Option<String>,
 }
 
+/// Volume specification used when running a container.
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct Volume {
+    /// Either a path on the host (if an absolute path) or the name of
+    /// a volume.
+    pub src: PathBuf,
+
+    /// Absolute path in the container where the volume will be
+    /// mounted.
+    pub dst: PathBuf,
+
+    /// If true, mount the volume read-write. Defaults to `false`.
+    pub read_write: bool,
+
+    /// Additional options to set on the volume.
+    pub options: Vec<String>,
+}
+
+impl Volume {
+    fn arg(&self) -> OsString {
+        let mut out = OsString::new();
+        out.push(&self.src);
+        out.push(":");
+        out.push(&self.dst);
+        if self.read_write {
+            out.push(":rw");
+        } else {
+            out.push(":ro");
+        }
+        for opt in &self.options {
+            out.push(",");
+            out.push(opt);
+        }
+        out
+    }
+}
+
 /// Options for running a container.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct RunOpt {
     /// Container image to run.
     pub image: String,
 
+    /// Optional name to give the container.
     pub name: Option<String>,
+
+    /// Volumes to mount in the container.
+    pub volumes: Vec<Volume>,
 
     /// Optional command to run.
     pub command: Option<PathBuf>,
--- tests/docker.rs
+++ tests/docker.rs
@@ -1,13 +1,17 @@
 use docker_command::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+fn new_path(s: &str) -> PathBuf {
+    Path::new(s).into()
+}
 
 #[test]
 fn test_build() {
     assert_eq!(
         Docker::new()
             .build(BuildOpt {
-                context: Path::new("/myContext").into(),
-                dockerfile: Some(Path::new("/myContext/myDockerfile").into()),
+                context: new_path("/myContext"),
+                dockerfile: Some(new_path("/myContext/myDockerfile")),
                 tag: Some("myTag".into()),
             })
             .command_line_lossy(),
@@ -20,12 +24,28 @@ fn test_run() {
     assert_eq!(
         Docker::new()
             .run(RunOpt {
-                name: Some("myName".into()),
                 image: "myImage".into(),
+                name: Some("myName".into()),
+                volumes: vec![
+                    // Read-write volume
+                    Volume {
+                        src: new_path("/mySrc"),
+                        dst: new_path("/myDst"),
+                        read_write: true,
+                        ..Default::default()
+                    },
+                    // Read-only volume with extra options
+                    Volume {
+                        src: new_path("/mySrc"),
+                        dst: new_path("/myDst"),
+                        options: vec!["cached".into(), "z".into()],
+                        ..Default::default()
+                    }
+                ],
                 command: Some(Path::new("myCmd").into()),
                 args: vec!["arg1".into(), "arg2".into()],
             })
             .command_line_lossy(),
-        "docker run --name myName myImage myCmd arg1 arg2"
+        "docker run --name myName --volume /mySrc:/myDst:rw --volume /mySrc:/myDst:ro,cached,z myImage myCmd arg1 arg2"
     );
 }
